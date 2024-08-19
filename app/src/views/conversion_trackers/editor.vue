bceaa30d4c6fb5eab5e35aaf4a6c432d43cc23cf
Emiroglu <emiremiroglu@gmail.com>
2024-08-19 14:54:38 +0300

    Mon Aug 19 14:54:38 +03 2024

--- app/src/views/conversion_trackers/editor.vue
+++ app/src/views/conversion_trackers/editor.vue
@@ -5,6 +5,7 @@
   import { VAceEditor } from 'vue3-ace-editor';
 
   import session from '@/lib/session';
+  import ace_config from '@/config/ace.json';
   import get from '@/lib/get';
   import shortid from 'shortid';
   
@@ -23,51 +24,6 @@
   const tracker = ref({})
   
   const title = ref('')
-
-  const options = ref({
-    // editor options
-    selectionStyle: 'line', // "line"|"text"
-    highlightActiveLine: false, // boolean
-    highlightSelectedWord: true, // boolean
-    readOnly: false, // boolean: true if read only
-    cursorStyle: 'ace', // "ace"|"slim"|"smooth"|"wide"
-    mergeUndoDeltas: true, // false|true|"always"
-    behavioursEnabled: true, // boolean: true if enable custom behaviours
-    wrapBehavioursEnabled: true, // boolean
-    autoScrollEditorIntoView: undefined, // boolean: this is needed if editor is inside scrollable page
-    keyboardHandler: null, // function: handle custom keyboard events
-    
-    // renderer options
-    animatedScroll: false, // boolean: true if scroll should be animated
-    displayIndentGuides: false, // boolean: true if the indent should be shown. See 'showInvisibles'
-    showInvisibles: false, // boolean -> displayIndentGuides: true if show the invisible tabs/spaces in indents
-    showPrintMargin: true, // boolean: true if show the vertical print margin
-    printMarginColumn: 80, // number: number of columns for vertical print margin
-    printMargin: undefined, // boolean | number: showPrintMargin | printMarginColumn
-    showGutter: true, // boolean: true if show line gutter
-    fadeFoldWidgets: false, // boolean: true if the fold lines should be faded
-    showFoldWidgets: true, // boolean: true if the fold lines should be shown ?
-    showLineNumbers: true,
-    highlightGutterLine: false, // boolean: true if the gutter line should be highlighted
-    hScrollBarAlwaysVisible: false, // boolean: true if the horizontal scroll bar should be shown regardless
-    vScrollBarAlwaysVisible: false, // boolean: true if the vertical scroll bar should be shown regardless
-    fontSize: 15, // number | string: set the font size to this many pixels
-    fontFamily: undefined, // string: set the font-family css value
-    maxLines: undefined, // number: set the maximum lines possible. This will make the editor height changes
-    minLines: undefined, // number: set the minimum lines possible. This will make the editor height changes
-    maxPixelHeight: 0, // number -> maxLines: set the maximum height in pixel, when 'maxLines' is defined. 
-    theme: 'ace/theme/dawn', // theme string from ace/theme or custom?
-
-    // session options
-    firstLineNumber: 1, // number: the line number in first line
-    overwrite: false, // boolean
-    newLineMode: 'auto', // "auto" | "unix" | "windows"
-    useSoftTabs: true, // boolean: true if we want to use spaces than tabs
-    tabSize: 2, // number
-    wrap: false, // boolean | string | number: true/'free' means wrap instead of horizontal scroll, false/'off' means horizontal scroll instead of wrap, and number means number of column before wrap. -1 means wrap at print margin
-    indentedSoftWrap: true, // boolean
-    mode: 'ace/mode/javascript' // string: path to language mode 
-  })
 
   onMounted(() => {
     if(id.value) {
@@ -101,7 +57,7 @@
       </div>
       <input class="w-full p-4" type="text" v-model="tracker.filename" disabled>
       <div class="relative flex-1">
-        <v-ace-editor class="absolute w-full h-full rounded" ref="editor" v-model:value="tracker.code" lang="javascript" :options="options" />
+        <v-ace-editor class="absolute w-full h-full rounded" ref="editor" v-model:value="tracker.code" lang="javascript" :options="ace_config" />
       </div>
     </section>
     <section v-if="loading" class="flex items-center justify-center absolute w-screen h-screen top-0 right-0 bottom-0 left-0">
--- app/src/views/dco/editor.vue
+++ app/src/views/dco/editor.vue
@@ -5,6 +5,7 @@
   import { VAceEditor } from 'vue3-ace-editor';
 
   import session from '@/lib/session';
+  import ace_config from '@/config/ace.json';
   import get from '@/lib/get';
   import shortid from 'shortid';
   
@@ -25,51 +26,6 @@
   const banner = ref({})
   
   const title = ref('')
-
-  const options = ref({
-    // editor options
-    selectionStyle: 'line', // "line"|"text"
-    highlightActiveLine: false, // boolean
-    highlightSelectedWord: true, // boolean
-    readOnly: false, // boolean: true if read only
-    cursorStyle: 'ace', // "ace"|"slim"|"smooth"|"wide"
-    mergeUndoDeltas: true, // false|true|"always"
-    behavioursEnabled: true, // boolean: true if enable custom behaviours
-    wrapBehavioursEnabled: true, // boolean
-    autoScrollEditorIntoView: undefined, // boolean: this is needed if editor is inside scrollable page
-    keyboardHandler: null, // function: handle custom keyboard events
-    
-    // renderer options
-    animatedScroll: false, // boolean: true if scroll should be animated
-    displayIndentGuides: false, // boolean: true if the indent should be shown. See 'showInvisibles'
-    showInvisibles: false, // boolean -> displayIndentGuides: true if show the invisible tabs/spaces in indents
-    showPrintMargin: true, // boolean: true if show the vertical print margin
-    printMarginColumn: 80, // number: number of columns for vertical print margin
-    printMargin: undefined, // boolean | number: showPrintMargin | printMarginColumn
-    showGutter: true, // boolean: true if show line gutter
-    fadeFoldWidgets: false, // boolean: true if the fold lines should be faded
-    showFoldWidgets: true, // boolean: true if the fold lines should be shown ?
-    showLineNumbers: true,
-    highlightGutterLine: false, // boolean: true if the gutter line should be highlighted
-    hScrollBarAlwaysVisible: false, // boolean: true if the horizontal scroll bar should be shown regardless
-    vScrollBarAlwaysVisible: false, // boolean: true if the vertical scroll bar should be shown regardless
-    fontSize: 15, // number | string: set the font size to this many pixels
-    fontFamily: undefined, // string: set the font-family css value
-    maxLines: undefined, // number: set the maximum lines possible. This will make the editor height changes
-    minLines: undefined, // number: set the minimum lines possible. This will make the editor height changes
-    maxPixelHeight: 0, // number -> maxLines: set the maximum height in pixel, when 'maxLines' is defined. 
-    theme: 'ace/theme/dawn', // theme string from ace/theme or custom?
-
-    // session options
-    firstLineNumber: 1, // number: the line number in first line
-    overwrite: false, // boolean
-    newLineMode: 'auto', // "auto" | "unix" | "windows"
-    useSoftTabs: true, // boolean: true if we want to use spaces than tabs
-    tabSize: 2, // number
-    wrap: false, // boolean | string | number: true/'free' means wrap instead of horizontal scroll, false/'off' means horizontal scroll instead of wrap, and number means number of column before wrap. -1 means wrap at print margin
-    indentedSoftWrap: true, // boolean
-    mode: 'ace/mode/html' // string: path to language mode 
-  })
 
   onMounted(() => {
     if(id.value) {
@@ -123,7 +79,7 @@
     </section>
     <section v-if="!loading" class="flex flex-col flex-1 relative h-screen">
       <div class="relative flex-1">
-        <v-ace-editor class="absolute w-full h-full" ref="editor" v-model:value="banner.html" lang="html" :options="options" />
+        <v-ace-editor class="absolute w-full h-full" ref="editor" v-model:value="banner.html" lang="html" :options="ace_config" />
       </div>
     </section>
     <section v-if="loading" class="flex items-center justify-center absolute w-screen h-screen top-0 right-0 bottom-0 left-0">
